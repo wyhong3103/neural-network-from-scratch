a708d1bb79b55edbc139f80ba9730a3d8a0b901d
wyhong3103 <wyhong3103@gmail.com>
2024-01-07 19:18:29 +0800

    Refactor layers

--- layers/fc_layer.py
+++ layers/fc_layer.py
@@ -1,5 +1,5 @@
-from auto_diff import Value
 import numpy as np
+from auto_diff import Value
 import random
 
 class FC_Layer:
@@ -8,56 +8,36 @@ class FC_Layer:
         self.activation = activation
 
     def init(self, prev_layer):
-        self.w = np.array(
-            [
-                [Value(random.random()) for _ in range(prev_layer.units)]
-                for _ in range(self.units)
-            ]
-        )
+        self.w = np.array([[Value(random.uniform(-1, 1)) for _ in range(prev_layer.units)] for _ in range(self.units)])
 
-        self.b = np.array(
-            [
-                Value(random.random()) for _ in range(self.units)
-            ]
-        )
+        self.b = np.array([Value(0) for _ in range(self.units)])
 
-        self.a = np.array(
-            [
-                Value(random.random()) for _ in range(self.units)
-            ]
-        )
+        self.a = np.array([Value(0) for _ in range(self.units)])
     
     def forward_prop(self, prev_layer):
+
         self.a = self.w @ prev_layer.a + self.b
+        
         if (self.activation == 'relu'):
-            for i in self.a:
-                i = i.relu()
+            for i in range(self.units):
+                self.a[i] = self.a[i].relu()
         elif (self.activation == 'sigmoid'):
-            for i in self.a:
-                i = i.sigmoid()
+            for i in range(self.units):
+                self.a[i] = self.a[i].sigmoid()
     
     def get_w_grad(self):
-        return np.array(
-            [
-                [j.grad for j in i]
-                for i in self.w
-            ]
-        )
+        return [[j.grad for j in i] for i in self.w]
 
     def get_b_grad(self):
-        return np.array(
-            [i.grad for i in self.b]
-        )
+        return [i.grad for i in self.b]
 
     def get_a(self):
-        return np.array(
-            [i.value for i in self.a]
-        )
+        return [i.value for i in self.a]
     
-    def learn(self, w_grad, b_grad, learning_rate):
+    def learn(self, learning_rate):
         for i in range(len(self.w)):
             for j in range(len(self.w[i])):
-                self.w[i][j].value -= learning_rate * w_grad[i][j]
+                self.w[i][j].value -= learning_rate * self.w[i][j].grad
     
         for i in range(len(self.b)):
-            self.b[i].value -= learning_rate * b_grad[i]
+            self.b[i].value -= learning_rate * self.b[i].grad
--- layers/input_layer.py
+++ layers/input_layer.py
@@ -1,14 +1,8 @@
 from auto_diff import Value
-import numpy as np
-import random
 
 class Input_Layer:
     def __init__(self, units):
         self.units = units
 
     def init(self):
-        self.a = np.array(
-            [
-                Value(random.random()) for _ in range(self.units)
-            ]
-        )
+        self.a = [Value(0) for _ in range(self.units)]
