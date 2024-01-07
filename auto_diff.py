c90ad339d18031f1bd5536d6bef11d8346c835d3
wyhong3103 <wyhong3103@gmail.com>
2024-01-07 19:17:31 +0800

    Add more operations and their derivatives

--- auto_diff.py
+++ auto_diff.py
@@ -2,7 +2,7 @@ import math
 
 class Value:
     def __init__(self, value, operator=None, dependencies=[], power = 0):
-        self.value = value
+        self.value = float(value)
         self.operator = operator
         self.dependencies = dependencies
         self.power = power
@@ -27,7 +27,7 @@ class Value:
             self.dependencies[1].grad += self.dependencies[0].value * self.grad
         elif self.operator == '/':
             self.dependencies[0].grad += (1/self.dependencies[1].value) * self.grad
-            self.dependencies[1].grad += (-self.dependencies[0].value/self.dependencies[1].value**2) * self.grad
+            self.dependencies[1].grad += (-self.dependencies[0].value/(self.dependencies[1].value**2)) * self.grad
         elif self.operator == '^':
             self.dependencies[0].grad += self.power * (self.dependencies[0].value ** (self.power - 1)) * self.grad
         elif self.operator == 'relu':
@@ -35,13 +35,20 @@ class Value:
         elif self.operator == 'sigmoid':
             # Derivative of the sigmoid function
             # https://math.stackexchange.com/a/1225116
-            self.dependencies[0].grad += (1 / (1 + math.exp(-self.dependencies[0].value))) * (1 - 1 / (1 + math.exp(-self.dependencies[0].value))) * self.grad
+            self.dependencies[0].grad += (1.0 / (1.0 + math.exp(-self.dependencies[0].value))) * (1.0 - 1.0 / (1.0 + math.exp(-self.dependencies[0].value))) * self.grad
+        elif self.operator == 'neg':
+            self.dependencies[0].grad += -self.grad
+        elif self.operator == 'log':
+            self.dependencies[0].grad += (1.0 / self.dependencies[0].value) * self.grad
     
     def __add__(self, x):
         return Value(self.value + x.value, '+', [self, x])
     
     def __sub__(self, x):
         return Value(self.value - x.value, '-', [self, x])
+
+    def __neg__(self):
+        return Value(-self.value, 'neg', [self])
 
     def __mul__(self, x):
         return Value(self.value * x.value, '*', [self, x])
@@ -52,11 +59,14 @@ class Value:
     def __pow__(self ,x):
         return Value(self.value**x, '^', [self], x)
     
+    def log(self):
+        return Value(math.log(self.value), 'log', [self])
+            
     def relu(self):
         return Value(max(0, self.value), 'relu', [self])
 
     def sigmoid(self):
-        return Value(1 / (1 + math.exp(-self.value)), 'sigmoid', [self])
+        return Value(1.0 / (1.0 + math.exp(-self.value)), 'sigmoid', [self])
 
     def dfs(self, topsort):
         self.vis = True
@@ -146,4 +156,43 @@ a[1][1] = w[1][1][0] * a[0][0] + b[1][1]
 a[2][0] = w[2][0][0] * a[1][0] + w[2][0][1] * a[1][1] + b[2][0]
 
 a[2][0].backward()
+"""
+
+"""
+import numpy as np
+
+x = np.array([Value(2), Value(1)])
+w = np.array([[Value(1), Value(1)], [Value(0.5), Value(0)]])
+b = np.array([Value(0), Value(1)])
+
+a1 = w @ x  + b
+a1 = np.array([i.relu() for i in a1])
+cost = np.sum(a1)
+
+x = np.array([Value(1.3), Value(1)])
+a1 = w@x + b
+a1 = np.array([i.relu() for i in a1])
+cost += np.sum(a1)
+
+cost.backward()
+
+print(cost.value)
+
+print(w[0][0].grad)
+
+x = np.array([Value(2), Value(1)])
+w = np.array([[Value(1+0.001), Value(1)], [Value(0.5), Value(0)]])
+b = np.array([Value(0), Value(1)])
+
+a1 = w @ x  + b
+a1 = np.array([i.relu() for i in a1])
+cost = np.sum(a1)
+
+x = np.array([Value(1.3), Value(1)])
+a1 = w@x + b
+a1 = np.array([i.relu() for i in a1])
+
+cost += np.sum(a1)
+
+print(cost.value)
 """
